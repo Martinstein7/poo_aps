6394353d1f845591a74b2459676b7c48490e7019
Willian <willianmartinsgomes@gmail.com>
2026-05-09 16:04:14 -0300

    Conexao com o BD estabelecida com sucesso

--- src/dao/Conexao.java
+++ src/dao/Conexao.java
@@ -1,4 +1,21 @@
 package dao;
+//importar funcionalidades de sql dentro do Java
+import java.sql.*;
 
 public class Conexao {
+    //puxa o arquivo do banco de dados que se encontra dentro da pasta
+    private static final String URL = "jdbc:sqlite:agua.db";
+
+    public static Connection getConexao(){
+        try {
+            Class.forName("org.sqlite.JDBC"); //tenta carregar o driver de conexão de bd
+            return DriverManager.getConnection(URL); //inicia a conexão
+        } catch (ClassNotFoundException e){
+            System.out.println("Driver do SQLite não encontrado: "+ e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+        }//os catchs foram feitos para caso de erro para realizar a conexão, seja ela por não encontrar ou não conectar
+        return null;
+    }
+
 }
