2258ac0ed4f32010bbb461083819f274eca08563
Willian <willianmartinsgomes@gmail.com>
2026-05-09 20:56:43 -0300

    Classes de leitura das info do BD criadas

--- src/dao/EstadoDAO.java
+++ src/dao/EstadoDAO.java
@@ -1,4 +1,33 @@
 package dao;
 
+import model.Estado;
+import java.sql.*;
+
 public class EstadoDAO {
+
+    public Estado buscarPorSigla(String sigla) {
+    String sql = "SELECT * FROM estado WHERE idestado = ?";
+
+    //try garantindo que a conexão ira fechar sozinha após a consulta
+    try (Connection connect = Conexao.getConexao();
+        PreparedStatement statement = connect.prepareStatement(sql)) {
+
+        statement.setString(1, sigla);
+        ResultSet result = statement.executeQuery();
+
+        if(result.next()){
+            //Utilizando os metodos contrutores criados
+            return new Estado(
+                    result.getString("idestado"),
+                    result.getDouble("cEsg"),
+                    result.getDouble("consumo_medio")
+            );
+        }
+    } catch (SQLException e) {
+        System.out.println("Erro ao buscar estado:" + e.getMessage());
+    }
+    return null;
+    }
 }
+
+
--- src/dao/FaixaConsumoDAO.java
+++ src/dao/FaixaConsumoDAO.java
@@ -1,4 +1,40 @@
 package dao;
 
+import java.sql.*;
+import model.FaixaConsumo;
+
 public class FaixaConsumoDAO {
+
+    public FaixaConsumo buscarPorEstado(String sigla) {
+        String sql = "SELECT * FROM faixa_consumo WHERE estado_idestado = ?";
+
+        try (Connection conn = Conexao.getConexao();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, sigla);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                return new FaixaConsumo(
+                        rs.getString("estado_idestado"),
+                        rs.getDouble("fixo"),
+                        rs.getInt("vol_inc"),
+                        rs.getDouble("step1"),
+                        rs.getInt("faixa1"),
+                        rs.getDouble("step2"),
+                        rs.getInt("faixa2"),
+                        rs.getDouble("step3"),
+                        rs.getInt("faixa3"),
+                        rs.getDouble("step4"),
+                        rs.getInt("faixa4"),
+                        rs.getDouble("step5"),
+                        rs.getInt("faixa5"),
+                        rs.getString("emp")
+                );
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro na FaixaConsumoDAO: " + e.getMessage());
+        }
+        return null;
+    }
 }
