ff44265eb0b7f5c5dfeaf824944773676ee7d898
Willian <willianmartinsgomes@gmail.com>
2026-05-09 22:21:36 -0300

    Classes que escrevem infos para o BD criadas

--- src/dao/ConsumoDAO.java
+++ src/dao/ConsumoDAO.java
@@ -1,4 +1,33 @@
 package dao;
 
+import model.Usuario;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.Consumo;
+import java.sql.*;
+
 public class ConsumoDAO {
+
+    public void registrarConsumo(Consumo consumo) {
+
+        String sql = "INSERT INTO consumo (m3gastos, dataleitura, usuario_idusuario) VALUES (?,?,?)";
+
+
+        try (Connection connect = Conexao.getConexao();
+             PreparedStatement statement = connect.prepareStatement(sql)) {
+
+
+            statement.setDouble(1, consumo.getM3Gastos());
+            statement.setString(2, consumo.getDataLeitura());
+            statement.setInt(3, consumo.getUsuarioIdUsuario());
+
+            statement.executeUpdate();
+            System.out.println("Consumo registrado!");
+        } catch (SQLException e) {
+            System.out.println("Erro ao registrar consumo:" + e.getMessage());
+        }
+    }
 }
--- src/dao/UsuarioDAO.java
+++ src/dao/UsuarioDAO.java
@@ -1,4 +1,27 @@
 package dao;
 
+import model.Usuario;
+import java.sql.*;
+
 public class UsuarioDAO {
+
+    public void registrarUsuario(Usuario usuario) {
+        //não é necessario utilizarmos o IDusuario que estava na outra classe pois ele gera sozinho
+        String sql = "INSERT INTO usuario (nomeusuario, numresidentes, idestado) VALUES (?,?,?)";
+
+
+        try (Connection connect = Conexao.getConexao();
+             PreparedStatement statement = connect.prepareStatement(sql)) {
+
+
+            statement.setString(1, usuario.getNomeUsuario());
+            statement.setInt(2, usuario.getNumResidentes());
+            statement.setString(3, usuario.getIdEstado());
+
+            statement.executeUpdate();//aqui atualizamos nosso BD
+            System.out.println("Usuario" + usuario.getNomeUsuario() + "salvo!");
+        } catch (SQLException e) {
+            System.out.println("Erro ao salvar o usuário:" + e.getMessage());
+        }
+    }
 }
