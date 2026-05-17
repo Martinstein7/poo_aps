718378b0dd40423b6ca3bfb425b4371792690df1
Willian <willianmartinsgomes@gmail.com>
2026-05-17 18:09:48 -0300

    atualizando classe main

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,50 @@
-import view.Interface;
-import com.formdev.flatlaf.FlatDarkLaf;
-import javax.swing.UIManager;
-import java.awt.Color;
+import dao.UsuarioDAO;
+import model.Usuario;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import view.controller.UserTabController;
+import java.util.List;
 
-public class Main {
-    public static void main(String[] args) {
-        try {
-            FlatDarkLaf.setup();//visual modo escuro
-            configurarEstiloGlobal();//estilizando botoes do pane estilo win10+
-            //abre a interface
-            java.awt.EventQueue.invokeLater(() -> {
-                try {
-                    Interface tela = new Interface();
-                    tela.setVisible(true);
-                } catch (Exception e) {
-                    System.err.println("Erro ao abrir a interface: " + e.getMessage());
-                }
-            });
+public class Main extends Application {
 
-        } catch (Exception ex) {
-            System.err.println("Falha ao iniciar o tema visual: " + ex.getMessage());
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root;
+
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        List<String> usuarios = usuarioDAO.listarNomesUsuarios();
+
+        if (!usuarios.isEmpty()) {
+            Usuario usuarioAtivo = usuarioDAO.buscarPorNome(usuarios.get(0));
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/poo_aps/UserTab.fxml"));
+            root = loader.load();
+
+            UserTabController userTabController = loader.getController();
+            userTabController.inicializarComUsuarioReal(usuarioAtivo);
+        } else {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/poo_aps/Registration.fxml"));
+            root = loader.load();
         }
+
+        Scene scene = new Scene(root, 900, 600);
+
+        try {
+            String cssPath = getClass().getResource("/poo_aps/style.css").toExternalForm();
+            scene.getStylesheets().add(cssPath);
+        } catch (NullPointerException e) {
+            System.out.println("Aviso: Arquivo style.css não encontrado.");
+        }
+
+        primaryStage.setTitle("POO_APS - Sistema de Monitoramento Hídrico");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
-    //definindo que os paineis de erro estejam da mesma cor do programa
-    private static void configurarEstiloGlobal() {
-        //definindo cores com base no tema do projeto :)
-        Color azulD = new Color(0, 120, 215); // Azul moderno
-        Color fundoEscuro = new Color(15, 15, 25);   // Preto azulado
-
-        UIManager.put("Button.arc", 999);            // Botões ovais
-        UIManager.put("Component.arc", 15);          // Campos de texto e combos
-        UIManager.put("TextComponent.arc", 15);
-        UIManager.put("CheckBox.arc", 5);
-
-        // Estilizando Pane
-        UIManager.put("OptionPane.background", fundoEscuro);
-        UIManager.put("Panel.background", fundoEscuro);
-        UIManager.put("OptionPane.messageForeground", Color.WHITE);
-
-        // cor dos botes
-        UIManager.put("Button.background", azulD);
-        UIManager.put("Button.foreground", Color.WHITE);
-        UIManager.put("Button.hoverBackground", azulD.brighter());
-
-
+    public static void main(String[] args) {
+        launch(args);
     }
 }
